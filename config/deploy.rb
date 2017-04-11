a17a692cdc01a6e01ffaeb300c08b3b6fa587765
oldseven <oldseven@nn.com>
2017-04-11 16:40:19 +0800

    modify ssh_options

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,13 +9,13 @@ set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
-
+# set :ssh_options, known_hosts: Net::SSH::KnownHosts
 # set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 # set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 # set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 # set :puma_access_log, "#{release_path}/log/puma.error.log"
 # set :puma_error_log,  "#{release_path}/log/puma.access.log"
-# set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 # set :puma_preload_app, true
 # set :puma_worker_timeout, nil
 
@@ -37,17 +37,17 @@ set :linked_dirs,  %w{log tmp/pids tmp/cache tmp/sockets }
 # in order to prevent bundler from overwriting the version controlled binstubs
 set :bundle_binstubs, nil
 
-# namespace :puma do
-#   desc 'Create Directories for Puma Pids and Socket'
-#   task :make_dirs do
-#     on roles(:app) do
-#       # execute "mkdir #{shared_path}/tmp/sockets -p"
-#       # execute "mkdir #{shared_path}/tmp/pids -p"
-#     end
-#   end
+namespace :puma do
+  # desc 'Create Directories for Puma Pids and Socket'
+  # task :make_dirs do
+  #   on roles(:app) do
+  #     # execute "mkdir #{shared_path}/tmp/sockets -p"
+  #     # execute "mkdir #{shared_path}/tmp/pids -p"
+  #   end
+  # end
 
-#   before :start, :make_dirs
-# end
+  # before :start, :make_dirs
+end
 
 namespace :deploy do
   desc "Make sure local git is in sync with remote."
