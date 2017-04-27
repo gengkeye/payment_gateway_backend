e9cd22d25e34512a259464af6f56749fbb4a1c09
oldseven <oldseven@nn.com>
2017-04-27 17:36:36 +0800

    restart puma after deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -56,8 +56,15 @@ namespace :deploy do
   desc 'Initial Deploy'
   task :initial do
     on roles(:app) do
-      # before 'deploy:restart', 'puma:start'
+      before 'deploy:restart', 'puma:start'
       invoke 'deploy'
+    end
+  end
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'puma:restart'
     end
   end
 
