5211c8bb996fc68cbecf9cf310d939aa177522ab
oldseven <oldseven@nn.com>
2017-05-08 15:19:50 +0800

    fix mail config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,9 +61,9 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
 
-  # config.action_mailer.default_url_options = { host: 'localhost', port: 7001 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 7001 }
 
-  # config.action_mailer.asset_host = 'http://admin.skyluster.com'
+  config.action_mailer.asset_host = 'http://admin.skyluster.com'
 
   config.action_mailer.delivery_method = :smtp
 
